f81c92754ec84118628a11e4053ee5b33b04175c
Adam Wróbel <Adam Wrbel>
2016-04-20 21:26:20 +0200

    Very basic, but working app.

---
+++ Mnemosyne-notes/Mnemosyne-notes.cpp
@@ -0,0 +1,244 @@
+#include "stdafx.h"
+#include "Mnemosyne-notes.h"
+#include <Windows.h>
+#include <shellapi.h>
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <iterator>
+
+#define ID_TRAY WM_USER+20
+#define IDM_CHANGE WM_USER+51
+#define WM_USER_SHELLICON WM_USER+1
+#define MAX_NOTE_SIZE 10000
+
+wchar_t note[MAX_NOTE_SIZE];
+NOTIFYICONDATA nid;
+HMENU hPopMenu;
+HWND hWnd;
+LPCWSTR	szTitle = L"Mnemosyne-notes";				// The title bar text
+LPCWSTR	szWindowClass = L"Mnemosyne-notes";			// the main window class name
+LPCWSTR	icon_path_name = L"icon.ico";
+
+HFONT font = CreateFont(20, 8, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH | FF_ROMAN, L"Tahoma");
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    Edit(HWND, UINT, WPARAM, LPARAM);
+
+
+//MAIN
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+
+	MyRegisterClass(hInstance);
+
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MNEMOSYNENOTES));
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return (int)msg.wParam;
+}
+
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MNEMOSYNENOTES));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = 0;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	DWORD styleEx = WS_EX_COMPOSITED | WS_EX_LAYERED | WS_EX_NOACTIVATE | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW;
+	DWORD style = WS_POPUP | WS_VISIBLE;
+	hWnd = CreateWindowEx(styleEx, szWindowClass, szTitle, style, GetSystemMetrics(SM_CXSCREEN) - 400, 10, 400, 800, NULL, NULL, hInstance, NULL);
+	SetLayeredWindowAttributes(hWnd, 0x00ffffff, 255, LWA_COLORKEY | LWA_ALPHA | SW_SHOWMINIMIZED);
+	SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE | SW_SHOWMINIMIZED);
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	DWORD icon_flags = LR_LOADFROMFILE | LR_DEFAULTSIZE;
+	HICON hicon = (HICON)LoadImage(hInstance, icon_path_name, IMAGE_ICON, 0, 0, icon_flags);
+
+	LPCWSTR sTip = L"Mnemosyne-notes";
+	nid.cbSize = sizeof(NOTIFYICONDATA);
+	nid.hWnd = (HWND)hWnd;
+	nid.uID = ID_TRAY;
+	nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+	nid.uCallbackMessage = WM_USER_SHELLICON;
+	nid.hIcon = hicon;
+	lstrcpy(nid.szTip, sTip);
+	Shell_NotifyIcon(NIM_ADD, &nid);
+	return TRUE;
+}
+
+POINT lpClickPoint;
+UINT uFlag;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE:
+		break;
+	case WM_USER_SHELLICON:
+		switch (LOWORD(lParam)) {
+		case WM_RBUTTONDOWN:
+			uFlag = MF_BYPOSITION | MF_STRING;
+			GetCursorPos(&lpClickPoint);
+			hPopMenu = CreatePopupMenu();
+			InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, IDM_CHANGE, _T("Change note"));
+			InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, IDM_EXIT, _T("Exit"));
+			SetForegroundWindow(hWnd);
+			TrackPopupMenu(hPopMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_BOTTOMALIGN, lpClickPoint.x, lpClickPoint.y, 0, hWnd, NULL);
+			break;
+
+		case WM_LBUTTONDOWN:
+			SetForegroundWindow(hWnd);
+			break;
+		}break;
+
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		switch (wmId)
+		{
+		case IDM_CHANGE:
+			DialogBox((HINSTANCE)GetWindowLong(hWnd,GWL_HINSTANCE), MAKEINTRESOURCE(IDD_DIALOG1), hWnd, Edit);
+			break;
+
+		case IDM_EXIT:
+			SendMessage(hWnd, WM_CLOSE, 0, 0);
+			break;
+
+		case IDM_ABOUT:
+			DialogBox((HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE), MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}break;
+
+	case WM_PAINT:
+	{
+		RECT rect;
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		SetTextColor(hdc, RGB(254, 254, 254));
+		GetClientRect(hWnd, &rect);
+		rect.left = 0;
+		rect.top = 0;
+		SelectObject(hdc, font);
+		DrawText(hdc, note, -1, &rect, DT_NOCLIP | DT_CENTER);
+		DeleteDC(hdc);
+		EndPaint(hWnd, &ps);
+	}
+	break;
+
+	case WM_CLOSE:
+		DestroyWindow(hWnd);
+		break;
+
+	case WM_DESTROY:
+		Shell_NotifyIcon(NIM_DELETE, &nid);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK Edit(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		SetDlgItemText(hDlg, IDC_EDIT1, note);
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+
+		case IDOK:
+			UpdateWindow(hWnd);
+			RECT rect;
+			GetClientRect(hWnd, &rect);
+			rect.left = 0;
+			rect.top = 0;
+			GetDlgItemText(hDlg, IDC_EDIT1, note, MAX_NOTE_SIZE);
+			InvalidateRect(hWnd, &rect, true);
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+			break;
+
+		case IDCANCEL:
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+			break;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
---
+++ Mnemosyne-notes/ReadMe.txt
@@ -0,0 +1,62 @@
+========================================================================
+    WIN32 APPLICATION : Mnemosyne-notes Project Overview
+========================================================================
+
+AppWizard has created this Mnemosyne-notes application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your Mnemosyne-notes application.
+
+
+Mnemosyne-notes.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+Mnemosyne-notes.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+Mnemosyne-notes.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+AppWizard has created the following resources:
+
+Mnemosyne-notes.rc
+    This is a listing of all of the Microsoft Windows resources that the
+    program uses.  It includes the icons, bitmaps, and cursors that are stored
+    in the RES subdirectory.  This file can be directly edited in Microsoft
+    Visual C++.
+
+Resource.h
+    This is the standard header file, which defines new resource IDs.
+    Microsoft Visual C++ reads and updates this file.
+
+Mnemosyne-notes.ico
+    This is an icon file, which is used as the application's icon (32x32).
+    This icon is included by the main resource file Mnemosyne-notes.rc.
+
+small.ico
+    This is an icon file, which contains a smaller version (16x16)
+    of the application's icon. This icon is included by the main resource
+    file Mnemosyne-notes.rc.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named Mnemosyne-notes.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
